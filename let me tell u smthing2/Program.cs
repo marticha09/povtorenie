25736488f4df71f16b8f83755b523b396acc78e5
marticha09 <hokinmartin590@gmail.com>
2024-12-13 09:08:08 +0200

    Add files via upload

---
+++ let me tell u smthing2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace let_me_tell_u_smthing2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ let me tell u something/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace let_me_tell_u_something
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //proverqva dali dve nakloneni vuvejdame 4islo ot 1 do 100
+            //ako 4isloto e pod 1 i nad 100 izvejda suobshtenie
+            Console.Write("vuvedi edno 4islo ");
+            var num = int.Parse(Console.ReadLine());
+            while (num > 1 & num < 100)
+            {
+                Console.WriteLine("lemme tell u something");
+                num = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("4isloto e:{0} i e pod diapazona 1-100", num);
+
+
+        }
+            
+           
+        
+    }
+}
